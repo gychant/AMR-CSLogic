12989705f4b522f0bb8c7fc68450e8946ce94622
moono <moono@jp.ibm.com>
2021-09-27 16:41:59 +0900

    refactoring; the content on amr_verbnet_semantics/corpus_readers/verbnet_reader.py was moved to amr_verbnet_semantics/corpus_readers/verbnet.py

--- amr_verbnet_semantics/corpus_readers/verbnet.py
+++ amr_verbnet_semantics/corpus_readers/verbnet.py
@@ -2,12 +2,49 @@
 VerbNet query wrapper
 """
 from nltk.corpus.util import LazyCorpusLoader
+from nltk.corpus.reader import VerbnetCorpusReader
 
 import config
 from amr_verbnet_semantics.corpus_readers.rdf_kb import \
     query_semantics_from_rdf
-from amr_verbnet_semantics.corpus_readers.verbnet_reader import \
-    VerbnetCorpusReaderEx
+
+
+class VerbnetCorpusReaderEx(VerbnetCorpusReader):
+    """
+    An extension class based on NLTK VerbnetCorpusReader to capture the negation of statement.
+    Reference: https://www.nltk.org/_modules/nltk/corpus/reader/verbnet.html
+    """
+
+    def __init__(self, root, fileids, wrap_etree=False):
+        VerbnetCorpusReader.__init__(self, root, fileids, wrap_etree)
+
+    def _get_semantics_within_frame(self, vnframe):
+        """Returns semantics within a single frame
+
+        A utility function to retrieve semantics within a frame in VerbNet
+        Members of the semantics dictionary:
+        1) Predicate value
+        2) Arguments
+
+        :param vnframe: An ElementTree containing the xml contents of
+            a VerbNet frame.
+        :return: semantics: semantics dictionary
+        """
+        semantics_within_single_frame = []
+        for pred in vnframe.findall("SEMANTICS/PRED"):
+            arguments = [
+                {"type": arg.get("type"), "value": arg.get("value")}
+                for arg in pred.findall("ARGS/ARG")
+            ]
+            semantics_within_single_frame.append(
+                {
+                    "predicate_value": pred.get("value"),
+                    "arguments": arguments,
+                    "is_negative": pred.get("bool", False) == "!"
+                }
+            )
+        return semantics_within_single_frame
+
 
 vn_dict = {
     "verbnet3.2": LazyCorpusLoader("verbnet3.2", VerbnetCorpusReaderEx, r"(?!\.).*\.xml"),
