84ea5434de0c7b189737474bf7cd5ebb9556c6c3
Rosario Uceda-Sosa <rosariou@us.ibm.com>
2021-09-27 21:45:46 -0400

    New version of propbank.
    Now make_out.12 is mapped.

--- KG/ulkb_access.py
+++ KG/ulkb_access.py
@@ -36,11 +36,11 @@ query_prefix =  ("prefix rrp: <http://www.ibm.com/RRP#> \n"
                 "prefix ulkb: <http://www.ibm.com/UL_KB#> \n")
 
 #THIS IS THE SERVER 
-sparql = SPARQLWrapper("http://goedel.sl.cloud9.ibm.com:9999/blazegraph/namespace/UL_KB_V1")
+sparql = SPARQLWrapper("http://goedel.sl.cloud9.ibm.com:9999/blazegraph/namespace/UL_KB_V2")
 #THIS IS A LOCAL VERSION (ROSARIO'S)
 #sparql = SPARQLWrapper("http://127.0.0.1:9999/blazegraph/namespace/UL_KB_V0")
 
-query_roles_for_pb_str = """SELECT DISTINCT ?verb ?pbSemRole ?vnVerbLabel ?vnParamLabel WHERE {{
+query_roles_for_pb_str = """SELECT DISTINCT ?verb ?pbSemRole ?vnVerbLabel ?vnVarType ?vnVarExpression WHERE {{
   ?verb rdfs:label "{}" . 
   #?verb rrp:inKB rrp:PropBank .
   ?verb rrp:hasParameter ?pbParam . 
@@ -51,9 +51,28 @@ query_roles_for_pb_str = """SELECT DISTINCT ?verb ?pbSemRole ?vnVerbLabel ?vnPar
   ?vnFrame rrp:hasComponent ?semPred . 
   ?semPred rrp:hasParameter ?vnParam . 
   ?pbParam rrp:mapsTo ?vnParam . 
-  ?vnParam rdfs:label ?vnParamLabel . 
+  ?vnParam rrp:varType ?vnVarType . 
+  ?vnParam rrp:varExpression ?vnVarExpression . 
   }}
 """
+
+query_roles_for_lemma_str = """SELECT DISTINCT ?pbSemRole ?vnVerbLabel ?vnVarType ?vnVarExpression WHERE {{  
+  ?entity rdfs:label "{}" . 
+  ?entity a rrp:Lemma . 
+  ?vnVerb rrp:hasComponent ?entity .
+  ?vnVerb rrp:inKB rrp:VerbNet . 
+  ?vnVerb rdfs:label ?vnVerbLabel . 
+  ?verb rrp:hasParameter ?pbParam . 
+  ?verb rrp:inKB rrp:PropBank. 
+  ?pbParam rdfs:label ?pbSemRole . 
+  ?vnVerb rrp:hasComponent ?vnFrame . 
+  ?vnFrame rrp:hasComponent ?semPred . 
+  ?semPred rrp:hasParameter ?vnParam . 
+  ?pbParam rrp:mapsTo ?vnParam . 
+  ?vnParam rrp:varType ?vnVarType . 
+  ?vnParam rrp:varExpression ?vnVarExpression . 
+  }} """
+
 query_all_parents_str = """SELECT DISTINCT ?entity ?entityLabel {{  
      <{}> wdt:P31* ?entityClass . 
      ?entityClass wdt:P279* ?entity . 
@@ -111,7 +130,6 @@ query_sem_predicates_str = """SELECT DISTINCT ?example ?operator ?semanticPredic
                 }} ORDER BY ?semanticPredicate
                   """
 
-
 query_check_verb_name_str = """SELECT DISTINCT ?entity ?provenance  WHERE {{
   {{ ?entity a rrp:Lemma }} UNION {{ ?entity a rrp:LinguisticClass }} 
   ?entity rdfs:label ?name . 
@@ -119,37 +137,57 @@ query_check_verb_name_str = """SELECT DISTINCT ?entity ?provenance  WHERE {{
   ?entity rrp:provenance ?provenance. 
 }} ORDER BY ?mapping"""                  
     
+###############################################################################
+# AUXILIARY FUNCTIONS
+###############################################################################
 
-#####################################################################
-# SEMANTIC ROLES FOR PROPBANK
-#####################################################################
-def ulkb_sem_roles_for(_pbName : str) -> {} : 
- 
- returnResults = []
- query_text = query_roles_for_pb_str.format(_pbName)
- sparql.setQuery(query_prefix + query_text) 
- sparql.setReturnFormat(JSON)
- results = sparql.query().convert()
- returnResults = {}
- #?verb ?pbSemRole ?vnVerbLabel ?vnParamLabel
- verbResults = {}
+def get_label(_entity : str) -> str : 
+    global WDURI
+    if WDURI not in _entity : 
+        _entity = WDURI + _entity
+    query = query_label_str.format(_entity)
+    r = requests.get(url_CURRENT, params = {'format': 'json', 'query': query})
+    resData = r.json()
+    for result in resData["results"]["bindings"]:
+       return result["entityLabel"]["value"]
+       #
+    return ""
+# Retrieves the equivalence classes for all entities. 
 
- for result in results["results"]["bindings"]:
-       pbSemRole = result["pbSemRole"]["value"]
-       vnVerbLabel = result["vnVerbLabel"]["value"]
-       vnParamLabel = result["vnParamLabel"]["value"]
-       if vnVerbLabel not in verbResults : 
-           verbResults[vnVerbLabel] = {}
-       curVerb = verbResults[vnVerbLabel]
-       if not pbSemRole in curVerb:
-           curVerb[pbSemRole]= vnParamLabel
-       #print(pbSemRole + "\t(" + vnVerbLabel + ", " + vnParamLabel + ")")
- returnResults["info"] = "semantic roles for " + _pbName
- returnResults[_pbName] = verbResults
- return returnResults
-      
+def analyze_coverage_text(_entries : {}, _output : str) : 
     
-
+    max = -1
+    counter = 0
+    outDict = {} 
+    for entry in _entries : 
+        if "COVID-19 pandemic in" in entry : 
+            continue
+        verbList = _entries[entry]
+        for rawVerb in verbList : 
+            verb = stem_verb(rawVerb)
+            counter += 1
+            if len(verb) == 0 : 
+                continue
+            if verb in outDict : 
+                continue
+            listItems = check_verb_name(verb)
+            if len(listItems) > 0 :
+                outDict[verb] = "YES"
+                print("process " + verb + ", YES")
+            else : 
+                outDict[verb] = "NO"
+                print("process " + verb + ", NO")
+        if max > 0 and counter > max : 
+                break
+            
+    with open(_output, 'w') as f: 
+        for key, value in outDict.items(): 
+            f.write('%s:%s\n' % (key, value))
+        #if verbList in ourDict:
+        #    continue
+     
+    f.close()
+    
 def link_to_terms_list(_list : []) -> []:
     
     resultList = []
@@ -182,10 +220,180 @@ def stem_verb(_inputVerb : str) -> str :
  ps =PorterStemmer()
  return ps.stem(_inputVerb)
 
+
+
+#####################################################################
+# SEMANTIC ROLES FOR PROPBANK OR LEMMAS
+#####################################################################
+def ulkb_sem_roles_for_pb(_pbName : str) -> {} : 
+ 
+ returnResults = []
+ query_text = query_roles_for_pb_str.format(_pbName)
+ sparql.setQuery(query_prefix + query_text) 
+ sparql.setReturnFormat(JSON)
+ results = sparql.query().convert()
+ returnResults = {}
+ #?verb ?pbSemRole ?vnVerbLabel ?vnParamLabel
+ verbResults = {}
+
+ for result in results["results"]["bindings"]:
+       pbSemRole = result["pbSemRole"]["value"]
+       vnVerbLabel = result["vnVerbLabel"]["value"]
+       vnVarType = result["vnVarType"]["value"]
+       vnVarExpression = result["vnVarExpression"]["value"]
+       if vnVerbLabel not in verbResults : 
+           verbResults[vnVerbLabel] = {}
+       curVerb = verbResults[vnVerbLabel]
+       if not pbSemRole in curVerb:
+           curVerb[pbSemRole]= vnVarType + "(" + vnVarExpression + ")"
+       #print(pbSemRole + "\t(" + vnVerbLabel + ", " + vnParamLabel + ")")
+ returnResults["info"] = "semantic roles for " + _pbName
+ returnResults[_pbName] = verbResults
+ return returnResults
+
+
+def ulkb_sem_roles_for_lemma(_lemma : str) -> {} : 
+ 
+ returnResults = []
+ query_text = query_roles_for_lemma_str.format(_lemma)
+ sparql.setQuery(query_prefix + query_text) 
+ sparql.setReturnFormat(JSON)
+ results = sparql.query().convert()
+ returnResults = {}
+ #?pbSemRole ?vnVerbLabel ?vnVarType ?vnVarExpression
+ verbResults = {}
+
+ for result in results["results"]["bindings"]:
+       pbSemRole = result["pbSemRole"]["value"]
+       vnVerbLabel = result["vnVerbLabel"]["value"]
+       
+       
+       vnVarType = result["vnVarType"]["value"]
+       vnVarExpression = result["vnVarExpression"]["value"]
+       if vnVerbLabel not in verbResults : 
+           verbResults[vnVerbLabel] = {}
+       curVerb = verbResults[vnVerbLabel]
+       if not pbSemRole in curVerb:
+           curVerb[pbSemRole]= vnVarType + "(" + vnVarExpression + ")"
+       #print(pbSemRole + "\t(" + vnVerbLabel + ", " + vnParamLabel + ")")
+ returnResults["info"] = "semantic roles for lemma " + _lemma
+ returnResults[_lemma] = verbResults
+ return returnResults
+                   
+                  
+#####################################################################
+# GET SEMANTIC ROLES FOR A VERB
+#####################################################################
+def ulkb_sem_predicates_for_vn_SHORT(_verb : str) -> [] : 
+    
+ global query_prefix
+ 
+ query_text = query_sem_predicates_str.format(_verb)
+ sparql.setQuery(query_prefix + query_text) 
+ sparql.setReturnFormat(JSON)
+ results = sparql.query().convert()
+
+ output = []
+ thisFrame = {}
+ output.append(thisFrame)
+ curPredicateID = ""
+ thisPredicate = {}
+ for result in results["results"]["bindings"]:
+       example = result["example"]["value"]
+       
+       if not 'example' in thisFrame : 
+            thisFrame['example'] = example
+       if example != thisFrame['example'] : 
+          thisFrame = {}         
+          output.append(thisFrame)
+          thisFrame['example'] = example 
+          curPredicateID = ""
+       thisPredicateID = result["semanticPredicate"]["value"]
+
+       
+       if 'predicates' not in thisFrame: 
+          thisFrame['predicates'] = []        
+       predicates = thisFrame['predicates']
+    
+       if thisPredicateID != curPredicateID : 
+           thisPredicate = {}
+           predicates.append(thisPredicate)
+           curPredicateID = thisPredicateID
+           
+       #predicateText = result["predicateText"]["value"]
+       predLabel = result["semanticPredicateLabel"]["value"]
+       
+       thisPredicate['label_predicate'] = predLabel
+       if "operator" in result :      
+           thisPredicate['operator'] = result["operator"]["value"] 
+           
+       if 'params' not in thisPredicate: 
+           thisPredicate['params'] = []
+       params = thisPredicate['params']
+       params.append(result["type"]["value"] + "(" + result["expression"]["value"] + ")")
+       #thisPredicate['type'] = result["type"]["value"]
+       #thisPredicate['value'] = result["value"]["value"]         
+       #print(example + " " + " " +  thisPredicateID + " " +  predLabel + " " +  
+       #      result["type"]["value"] + " " + result["value"]["value"] )
+ return output            
+
+def ulkb_sem_predicates_for_lemma_SHORT(_verb : str) -> [] : 
+    
+ global query_prefix
+ 
+ query_text = query_sem_predicates_str.format(_verb)
+ sparql.setQuery(query_prefix + query_text) 
+ sparql.setReturnFormat(JSON)
+ results = sparql.query().convert()
+
+ output = []
+ thisFrame = {}
+ output.append(thisFrame)
+ curPredicateID = ""
+ thisPredicate = {}
+ for result in results["results"]["bindings"]:
+       example = result["example"]["value"]
+       
+       if not 'example' in thisFrame : 
+            thisFrame['example'] = example
+       if example != thisFrame['example'] : 
+          thisFrame = {}         
+          output.append(thisFrame)
+          thisFrame['example'] = example 
+          curPredicateID = ""
+       thisPredicateID = result["semanticPredicate"]["value"]
+
+       
+       if 'predicates' not in thisFrame: 
+          thisFrame['predicates'] = []        
+       predicates = thisFrame['predicates']
+    
+       if thisPredicateID != curPredicateID : 
+           thisPredicate = {}
+           predicates.append(thisPredicate)
+           curPredicateID = thisPredicateID
+           
+       #predicateText = result["predicateText"]["value"]
+       predLabel = result["semanticPredicateLabel"]["value"]
+       
+       thisPredicate['label_predicate'] = predLabel
+       if "operator" in result :      
+           thisPredicate['operator'] = result["operator"]["value"] 
+           
+       if 'params' not in thisPredicate: 
+           thisPredicate['params'] = []
+       params = thisPredicate['params']
+       params.append(result["type"]["value"] + "(" + result["expression"]["value"] + ")")
+       #thisPredicate['type'] = result["type"]["value"]
+       #thisPredicate['value'] = result["value"]["value"]         
+       #print(example + " " + " " +  thisPredicateID + " " +  predLabel + " " +  
+       #      result["type"]["value"] + " " + result["value"]["value"] )
+ return output                      
+
 #####################################################################
 # MAP PROPBANK AND SEMANTIC ROLES TO VERBNET
 #####################################################################
-def ulkb_sem_predicates_for(_verb : str) -> [] : 
+def ulkb_sem_predicates_for_vn(_verb : str) -> [] : 
     
  global query_prefix
  
@@ -237,114 +445,11 @@ def ulkb_sem_predicates_for(_verb : str) -> [] :
        #thisPredicate['value'] = result["value"]["value"]         
        #print(example + " " + " " +  thisPredicateID + " " +  predLabel + " " +  
        #      result["type"]["value"] + " " + result["value"]["value"] )
- return output                 
-                  
-#####################################################################
-# MAP PROPBANK AND SEMANTIC ROLES TO VERBNET -- SHORT
-#####################################################################
-def ulkb_sem_predicates_for_SHORT(_verb : str) -> [] : 
-    
- global query_prefix
- 
- query_text = query_sem_predicates_str.format(_verb)
- sparql.setQuery(query_prefix + query_text) 
- sparql.setReturnFormat(JSON)
- results = sparql.query().convert()
-
- output = []
- thisFrame = {}
- output.append(thisFrame)
- curPredicateID = ""
- thisPredicate = {}
- for result in results["results"]["bindings"]:
-       example = result["example"]["value"]
-       
-       if not 'example' in thisFrame : 
-            thisFrame['example'] = example
-       if example != thisFrame['example'] : 
-          thisFrame = {}         
-          output.append(thisFrame)
-          thisFrame['example'] = example 
-          curPredicateID = ""
-       thisPredicateID = result["semanticPredicate"]["value"]
-
-       
-       if 'predicates' not in thisFrame: 
-          thisFrame['predicates'] = []        
-       predicates = thisFrame['predicates']
-    
-       if thisPredicateID != curPredicateID : 
-           thisPredicate = {}
-           predicates.append(thisPredicate)
-           curPredicateID = thisPredicateID
-           
-       #predicateText = result["predicateText"]["value"]
-       predLabel = result["semanticPredicateLabel"]["value"]
-       
-       thisPredicate['label_predicate'] = predLabel
-       if "operator" in result :      
-           thisPredicate['operator'] = result["operator"]["value"] 
-           
-       if 'params' not in thisPredicate: 
-           thisPredicate['params'] = []
-       params = thisPredicate['params']
-       params.append(result["type"]["value"] + "(" + result["expression"]["value"] + ")")
-       #thisPredicate['type'] = result["type"]["value"]
-       #thisPredicate['value'] = result["value"]["value"]         
-       #print(example + " " + " " +  thisPredicateID + " " +  predLabel + " " +  
-       #      result["type"]["value"] + " " + result["value"]["value"] )
- return output                 
-
+ return output    
 
 
     
     
-def get_label(_entity : str) -> str : 
-    global WDURI
-    if WDURI not in _entity : 
-        _entity = WDURI + _entity
-    query = query_label_str.format(_entity)
-    r = requests.get(url_CURRENT, params = {'format': 'json', 'query': query})
-    resData = r.json()
-    for result in resData["results"]["bindings"]:
-       return result["entityLabel"]["value"]
-       #
-    return ""
-# Retrieves the equivalence classes for all entities. 
-
-def analyze_coverage_text(_entries : {}, _output : str) : 
-    
-    max = -1
-    counter = 0
-    outDict = {} 
-    for entry in _entries : 
-        if "COVID-19 pandemic in" in entry : 
-            continue
-        verbList = _entries[entry]
-        for rawVerb in verbList : 
-            verb = stem_verb(rawVerb)
-            counter += 1
-            if len(verb) == 0 : 
-                continue
-            if verb in outDict : 
-                continue
-            listItems = check_verb_name(verb)
-            if len(listItems) > 0 :
-                outDict[verb] = "YES"
-                print("process " + verb + ", YES")
-            else : 
-                outDict[verb] = "NO"
-                print("process " + verb + ", NO")
-        if max > 0 and counter > max : 
-                break
-            
-    with open(_output, 'w') as f: 
-        for key, value in outDict.items(): 
-            f.write('%s:%s\n' % (key, value))
-        #if verbList in ourDict:
-        #    continue
-     
-    f.close()
 
 
 if __name__ == '__main__':
@@ -353,15 +458,17 @@ if __name__ == '__main__':
     #print(str(check_verb_name("MIMI")))
     
     #TEST SEMANTIC ROLES
-    # output = ulkb_sem_roles_for("put.01") 
-    
+    #output = ulkb_sem_roles_for_pb("put.01") 
+    #print(str(output))
     #TEST SEMANTIC PREDICATES
-    output = ulkb_sem_predicates_for("escape-51.1-1-1")
-    print(str(output))
+    #output = ulkb_sem_predicates_for("escape-51.1-1-1")
+    #print(str(output))
      
-    output = ulkb_sem_predicates_for_SHORT("escape-51.1-1-1")
+    #output = ulkb_sem_predicates_for_SHORT("escape-51.1-1-1")
+    #print(str(output))
+    #TEST API WITH LEMMAS
+    output  = ulkb_sem_roles_for_lemma("make_out")
     print(str(output))
-    
                                 
     
     
