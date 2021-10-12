2fb869916e423db096c3cd1bc8299d8d85a67ab5
Masaki Ono <moono@jp.ibm.com>
2021-10-12 11:55:18 +0900

    added the cache function for AMR

--- amr_verbnet_semantics/service/amr.py
+++ amr_verbnet_semantics/service/amr.py
@@ -1,6 +1,12 @@
 """Wrappers for local and remote AMR parsing client"""
+import collections
+import copy
+import datetime
+import glob
 import os
+import pickle
 import sys
+import threading
 
 from nltk.tokenize import word_tokenize
 from nltk import sent_tokenize
@@ -58,6 +64,79 @@ class RemoteAMRClient(object):
         return self.parser.get_amr(text)
 
 
+class CacheClient:
+    def __init__(self, amr_client, cache_capacity=5000, delete_ratio=0.2,
+                 snapshot_interval=1000, use_snapshot=True):
+        self.amr_client = amr_client
+        self.delete_ratio = delete_ratio
+        self.capacity = cache_capacity
+        self.snapshot_prefix = 'snapshot.pickle_'
+        self.snapshot_interval = snapshot_interval
+        self.count = 0
+        self.cache = collections.OrderedDict()
+        if use_snapshot:
+            self.cache = self._read_snapshot()
+
+
+    def _read_snapshot(self):
+        dt_name_pairs = []
+        i = len(self.snapshot_prefix)
+        for name in glob.glob(f'*/{self.snapshot_prefix}*'):
+            try:
+                dt = datetime.datetime.strptime(os.path.basename(name)[i:],
+                                                '%Y-%m-%d_%H-%M-%S')
+                dt_name_pairs.append((dt, name))
+            except:
+                # do not handle an exception from datetime.datetime.strptime
+                pass
+
+        try:
+            if dt_name_pairs:
+                dt_name_pairs.sort(key=lambda x: x[0])
+                latest_name = dt_name_pairs[-1][1]
+                self.cache = pickle.load(open(latest_name, 'rb'))
+        except Exception as e:
+            print(e)
+
+    def get_amr(self, sentence):
+        # use cache
+        if sentence in self.cache:
+            amr = self.cache[sentence]
+            self.cache.move_to_end(sentence, last=False)  # move key "sentence" to the head
+
+        else:
+            amr = self.amr_client.get_amr(text=sentence)
+            self.cache[sentence] = amr
+
+            # delete unused keys
+            if len(self.cache) == self.capacity:
+                n = int(self.delete_ratio * self.capacity)
+                for _ in range(n):
+                    self.cache.popitem(last=True)
+
+        # save snapshot
+        self.count += 1
+        if self.count == self.snapshot_interval:
+            self.count = 0
+            now = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+            file_name = f'{self.snapshot_prefix}{now}'
+            thread = threading.Thread(target=self.save_snapshot,
+                                      args=(file_name,
+                                            copy.deepcopy(self.cache)))
+            thread.start()
+
+        return amr
+
+    @staticmethod
+    def save_snapshot(file_name, cache):
+        try:
+            pickle.dump(cache, open(file_name, 'wb'))
+            print(f'cache is saved on {file_name}')
+        except Exception as e:
+            print(e)
+
+
+
 if config.AMR_PARSING_MODEL == "local":
     amr_client = LocalAMRClient(config.use_cuda)
 elif config.AMR_PARSING_MODEL == "remote":
@@ -65,6 +144,7 @@ elif config.AMR_PARSING_MODEL == "remote":
 else:
     raise Exception("Missing AMR parsing configuration ...")
 
+amr_client = CacheClient(amr_client)
 
 def parse_text(text, verbose=False):
     sentences = sent_tokenize(text)
