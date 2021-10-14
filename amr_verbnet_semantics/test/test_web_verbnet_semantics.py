7485d9540da65e2c7f1951378adc67a5e69aa503
Masaki Ono <moono@jp.ibm.com>
2021-10-14 14:00:24 +0900

    updated a sample sentence on test code and removed a rest server

--- amr_verbnet_semantics/test/test_web_verbnet_semantics.py
+++ amr_verbnet_semantics/test/test_web_verbnet_semantics.py
@@ -4,7 +4,7 @@ from app_config import config
 
 if __name__ == '__main__':
 
-    text = "You're in a corridor. You can see a shoe cabinet."
+    text = 'The dresser is made out of maple carefully finished with Danish oil'
     params = {'text': text, 'use_coreference': 0}
     endpoint = f'http://{config.LOCAL_SERVICE_HOST}:{config.LOCAL_SERVICE_PORT}/verbnet_semantics'
     r = requests.get(endpoint, params=params)
